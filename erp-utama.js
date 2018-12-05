6b8d8cd203b309ca178bca7b9e5ca4085244a314
riyad12 <riyadhidayat1993@gmail.com>
2018-12-05 23:36:42 +0700

    Update erp-utama.js

--- erp-utama.js
+++ erp-utama.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/08/top-tips-for-sap-s4hana-erp.html"}
-if(x=="2"){xxx="/2018/08/tips-to-get-more-value-out-of-your.html"}
-if(x=="3"){xxx="/2018/08/putting-e-enterprise-back-into-erp.html"}
-if(x=="4"){xxx="/2018/08/five-erp-software-trends-to-watch-in.html"}
-if(x=="5"){xxx="/2018/08/five-common-gaps-to-watch-for-in-your.html"}a_to_ve[i].href="http://www.erp-software.me"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/09/top-five-things-to-do-prior-to.html"}
+if(x=="2"){xxx="/2018/09/your-erp-implementation-best-practices.html"}
+if(x=="3"){xxx="/2018/09/five-erp-software-trends-to-watch-in.html"}
+if(x=="4"){xxx="/2018/09/five-surprising-takeaways-from-2018-erp.html"}
+if(x=="5"){xxx="/2018/09/top-predictions-for-erp-software.html"}a_to_ve[i].href="http://www.revnetwork.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
